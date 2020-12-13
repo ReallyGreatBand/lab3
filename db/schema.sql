e4ff33dbaeba04a6b941e42bcbaeb2bde0725a7d
V1ckeyR <not.c0mp.uter.cat@gmail.com>
2020-12-13 20:45:35 +0200

    Change names in scheme

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
-set search_path to "Plants"; -- select "Plants" scheme
+-- Select "plants" scheme
+set search_path to plants;
 
--- Create tables.
-drop table if exists "Plants";
-create table "Plants"
+-- Create table
+create table if not exists plants
 (
-    id serial unique not null,
-    "soilMoistureLevel" real not null,
-    "soilDataTimestamp" timestamp not null
+	id serial not null
+		constraint table_name_id_key
+			unique,
+	soil_moisture_level real not null,
+	soil_data_timestamp timestamp not null
 );
 
 -- Create update soilDataTimestamp trigger
@@ -14,8 +16,8 @@ create or replace function aft_update()
   returns trigger as
 $$
 begin
-update "Plants"
-    set "soilDataTimestamp" = now()
+update plants
+    set soil_data_timestamp = now()
     where id = old.id;
 return null;
 end;
@@ -23,17 +25,17 @@ end;
 $$
 language 'plpgsql';
 
-drop trigger update_time on "Plants";
-create trigger update_time after update of "soilMoistureLevel" on "Plants"
+drop trigger if exists update_time on plants;
+create trigger update_time after update of soil_moisture_level on plants
     for each row
     execute procedure aft_update();
 
--- Insert demo data.
-insert into "Plants" (id, soilMoistureLevel, soilDataTimestamp)
+-- Insert demo data
+insert into plants (id, soil_moisture_level, soil_data_timestamp)
 values (default, 0.15, '2020-11-29 14:05:26.000000');
 
-insert into "Plants" (id, soilMoistureLevel, soilDataTimestamp)
+insert into plants (id, soil_moisture_level, soil_data_timestamp)
 values (default, 0.23, '2020-11-29 13:02:39.000000');
 
-insert into "Plants" (id, soilMoistureLevel, soilDataTimestamp)
+insert into plants (id, soil_moisture_level, soil_data_timestamp)
 values (default, 0.56, '2020-11-29 05:24:41.000000');
